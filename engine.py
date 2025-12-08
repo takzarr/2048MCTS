eafb9ad75963641c767bb8315e138efc8e4e1c7f
Your Name <your@email.com>
2025-12-08 14:24:18 -0800

    adding comments to 2048 game, removing commented code

--- engine.py
+++ engine.py
@@ -4,74 +4,49 @@ import math
 
 # GAME ENGINE CODE BELOW
 
+# number of rows and cols of 2048 grid
 rows, cols = 4, 4
+# actions of 2048 game
 ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-#grid = [[0 for _ in range(cols)] for _ in range(rows)]
-#grid[1] = [2,2,0,2]
-# grid[0][0] = 2
-# grid[1][0] = 2
-# grid[2][0] = 2
-# grid[3][0] = 2
-
 
 #create an instance of the grid with two tiles
 def new_grid():
+    # creates a list of lists. The list contains 'rows' lists,
+    # where each list contains 'cols' values, with the value 0
     grid = [[0 for _ in range(cols)] for _ in range(rows)]
+    # add a value 2 or 4 to a empty tile
     new_tiles(grid)
+    # add a value 2 or 4 to a empty tile
     new_tiles(grid)
     return grid
 
 def new_tiles(grid):
+    # get random value between 0 (inclusive) and 1 (exclusive)
     chance = random.random()
+    # add value 2 to a empty tile if chance is less than 0.9,
+    # and, otherwise, add value 4 to a empty tile.
     if chance < 0.9:
         spawn_tiles(grid, 2)
     else: 
         spawn_tiles(grid, 4)
-'''
-##check gameover
-grid[0][0] = 2
-grid[0][1] = 4
-grid[0][2] = 2
-grid[0][3] = 4
 
-grid[1][0] = 4
-grid[1][1] = 2
-grid[1][2] = 4
-grid[1][3] = 2
-
-grid[2][0] = 2
-grid[2][1] = 4
-grid[2][2] = 2
-grid[2][3] = 4
-
-grid[3][0] = 4
-grid[3][1] = 2
-grid[3][2] = 4
-grid[3][3] = 2
-'''
-
-#Check for empty tiles and place valie if not empty
+#Check for empty tiles and place value if not empty
 def spawn_tiles(grid, value):
+    # creates a list of tuples. Each tuple contains the row (r) index
+    # and the column (c) of the grid tile position, for tiles that 
+    # contain the value 0
     empty = [(r,c) for r in range(rows) for c in range(cols) if grid[r][c] == 0]
     if not empty:
         return
     r,c = random.choice(empty)
     grid[r][c] = value
-    '''
-    while True:
-        num1 = random.randint(0,3)
-        num2 = random.randint(0,3)
-        if not grid[num1][num2]:
-            grid[num1][num2] = value
-            break
-    '''
 
+# prints the grid
 def print_grid(grid):
-    for i in grid:
-        print(i)
-    print("\n\n")
-
-
+    for r in grid:
+        for c in r:
+            print(f"{c}", end='')
+        print("\n", end='')
 
 #STEPS
 # shift row
@@ -83,41 +58,43 @@ def right(grid):
     total_gain = 0
     moved = False
 
+    # for every row
     for r in range(rows):
+        # give me the current row list
         original = g[r][:]
+        # move non-zero values to the end of list
         shift_row_right(g[r])
+        # combines tiles
         total_gain += combine_row_right(g[r])
+        # move non-zero values to the end of list
         shift_row_right(g[r])
+        # set moved to true if one row's tiles changes value
         if g[r] != original:
             moved = True
     return g, total_gain, moved
-    '''
-    for i in range(4):
-        shift_row_right(grid[i])
-        combine_row_right(grid[i])
-        shift_row_right(grid[i])
-    '''
+
 #Shift non zero elements to the right
 def shift_row_right(row):
+    # create a new list that contains all
+    # non-zero values from row list.
     new_row = [x for x in row if x != 0]
+    # create a new list that appends all zero values
+    # from the row list before the list of non-zero values
     new_row = [0] * (len(row) - len(new_row)) + new_row
     row[:] = new_row #modify in place
-    '''
-    for i in range(3):
-        for j in range(3):
-            if not row[j + 1]:
-                row[j+1] = row[j]
-                row[j] = 0
-    '''
 
 #Combine row_right
 def combine_row_right(row):
     score_gain = 0
+    # for row indexes 3 to 1
     for i in range(3, 0, -1):
+        # if tile i is non-zero and tile i is equal to tile i-1
         if row[i] != 0 and row[i] == row[i-1]:
-            #row[i] += row[i-1]
+            # multiply tile value by 2
             row[i] *= 2
+            # add tile value to score 
             score_gain += row[i]
+            # set tile i-1 to 0
             row[i-1] = 0
     return score_gain
 
@@ -126,41 +103,42 @@ def left(grid):
     total_gain = 0
     moved = False
 
+    # for every row
     for r in range(rows):
+        # give me the current row list
         original = g[r][:]
+        # move non-zero values to beginning of list
         shift_row_left(g[r])
+        # combines tiles
         total_gain += combine_row_left(g[r])
+        # move non-zero values to beginning of list
         shift_row_left(g[r])
+        # set moved to true if one row's tiles changes value
         if g[r] != original:
             moved = True
     return g, total_gain, moved
 
-    '''
-    for i in range(4):
-        shift_row_left(grid[i])
-        combine_row_left(grid[i])
-        shift_row_left(grid[i])
-    '''
-
 #Shift non zero elements to the left
 def shift_row_left(row):
+    # create a new list that contains all
+    # non-zero values from row list.
     new_row = [x for x in row if x != 0]
+    # append all zero values from the row list after
+    # the list of non-zero values
     new_row += [0] * (len(row) - len(new_row))
     row[:] = new_row #modify in place
-    '''
-    for i in range(3):
-        for j in range(3):
-            if not row[j]:
-                row[j] = row[j + 1]
-                row[j + 1] = 0
-    '''
+
 def combine_row_left(row):
     score_gain = 0
+    # for row indexes 0 to 3
     for i in range(3):
+        # if tile i is non-zero and tile i is equal to tile i+1
         if row[i] != 0 and row[i] == row[i+1]:
-            #row[i] += row[i+1]
+            # multiply tile value by 2
             row[i] *= 2
+            # add tile value to score 
             score_gain += row[i]
+            # set tile i+1 to 0
             row[i+1] = 0
     return score_gain
 
@@ -168,67 +146,25 @@ def transpose(grid):
     return [list(row) for row in zip(*grid)]
 
 def up(grid):
+    # tranpose the grid so that columns become rows
+    # and rows become columns
     transposed = transpose(grid)
+    # do left action on tranpose grid
     move_board, total_gain, moved = left(transposed)
+    # tranpose the tranpose grid to get original grid
     new_grid = transpose(move_board)
     return new_grid, total_gain, moved
-    '''
-    for i in range(4):
-        shift_col_up(i)
-        combine_col_up(i)
-        shift_col_up(i)
-    '''
-
-'''
-def shift_col_up(col):
-    for i in range(3):
-        for row in range(3):
-            if not grid[row][col]:
-                grid[row][col] = grid[row+1][col]
-                grid[row+1][col] = 0
-
-def combine_col_up(col):
-    for row in range(3):
-        if grid[row][col] == grid[row+1][col]:
-            grid[row][col] += grid[row+1][col]
-            grid[row+1][col] = 0
-'''
 
 def down(grid):
+    # tranpose the grid so that columns become rows
+    # and rows become columns
     transposed = transpose(grid)
+    # do right action on tranpose grid
     move_board, total_gain, moved = right(transposed)
+    # tranpose the tranpose grid to get original grid
     new_grid = transpose(move_board)
     return new_grid, total_gain, moved
 
-'''
-def shift_col_down(col):
-    for i in range(3):
-        for row in range(3,0,-1):
-            if not grid[row][col]:
-                grid[row][col] = grid[row-1][col]
-                grid[row-1][col] = 0
-def combine_col_down(col):
-    for row in range(3,0,-1):
-        if grid[row][col] == grid[row-1][col]:
-            grid[row][col] += grid[row-1][col]
-            grid[row-1][col] = 0
-    
-def gameover():
-
-    if not check_4d(1,1) and not check_4d(1,2) and not check_4d(2,1) and not check_4d(2,2):
-        if grid[0][0] != grid[1][0] and grid[0][0] != grid[0][1] and \
-        grid[3][0] != grid[2][0] and grid[3][0] != grid[3][1] and \
-        grid[0][3] != grid[0][2] and grid[0][3] != grid[1][3] and \
-        grid[3][3] != grid[3][2] and grid[3][3] != grid[2][3]:
-            print("Gameover")
-def check_4d(row,col):
-    if grid[row][col] != grid[row+1][col] and \
-    grid[row][col] != grid[row-1][col] and \
-    grid[row][col] != grid[row][col+1] and \
-    grid[row][col] != grid[row][col-1]:
-        return False
-    
-'''
 #apply the correct action to the grid and return the new grid, score gain, and whether any tile moved.
 def apply_action(grid, action):
     if action == "LEFT":
@@ -241,7 +177,7 @@ def apply_action(grid, action):
         new_grid_, score_gain, moved = down(grid)
     else:
         raise ValueError(f"Unknown action: {action}")
-
+    # spawn a new random tile if grid tiles moved
     if moved:
         new_tiles(new_grid_)  # spawn a new random tile
     return new_grid_, score_gain, moved
@@ -255,33 +191,9 @@ def get_valid_actions(grid):
             valid.append(action)
     return valid
 
-#check for number of valid actions nonemeans gameover
+#check for number of valid actions (no actions means gameover)
 def gameover(grid):
     return len(get_valid_actions(grid)) == 0
 
 def max_tile(grid):
     return max(max(row) for row in grid)    
-
-
-def main():
-    # #Starting tiles
-    # spawn_tiles(2); spawn_tiles(2); print_grid()
-
-    # down(); print_grid()
-    # #new_tiles(); print_grid()
-    # up(); print_grid()
-    # #new_tiles(); print_grid()
-    #gameover()
-    pass
-
-
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-            
-
-
